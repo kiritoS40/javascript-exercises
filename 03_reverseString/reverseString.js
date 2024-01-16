941cd6e7dc0ff7b27578daa420cf7a6c86fff1dd
Mark David Prado <155267500+kiritoS40@users.noreply.github.com>
2024-01-16 16:57:03 +0800

    Implement reverseString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  //   let reversedString = "";
+  //   for (let i = string.length - 1; i >= 0; i--) {
+  //     reversedString += string[i];
+  //   }
+  //   return reversedString;
 
+  // A more efficient solution
+  return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
