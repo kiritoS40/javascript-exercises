6b40764f090a9ff993836218ea3f54e38d3373e5
Mark David Prado <155267500+kiritoS40@users.noreply.github.com>
2024-01-25 21:55:29 +0800

    Implement the function palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  const length = processedString.length;
 
+  for (let i = 0; i < length / 2; i++) {
+    if (processedString[i] !== processedString[length - 1 - i]) return false;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
