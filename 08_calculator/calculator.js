453951c7182e593547861cbc1467741f701f5502
Mark David Prado <155267500+kiritoS40@users.noreply.github.com>
2024-01-25 21:39:20 +0800

    Implement the operator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numArray) {
+  return numArray.reduce((acc, currentValue) => acc + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numArray) {
+  return numArray.reduce((acc, currentValue) => acc * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +30,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
